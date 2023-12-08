cf7af7e8cbb68125aa9500a97f70c222025ed973
Werner Marx <werner.marx@outlook.com>
2023-12-07 19:29:01 -0700

    added tests

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,7 +2,7 @@
 Calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     """bla bla bla"""
     return x + y
-    
--- app/app/test.py
+++ app/app/test.py
@@ -2,10 +2,10 @@ from django.test import SimpleTestCase
 
 from app import calc
 
-class CalcTests(SimpleTestCase):
 
+class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
 
-        res = calc.add(5,7)
+        res = calc.add(5, 7)
 
         self.assertEqual(res, 12)
