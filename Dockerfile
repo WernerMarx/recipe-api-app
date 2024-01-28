a3fb950d60abcb1b49a07d533b51432688c0a751
Werner Marx <werner.marx@outlook.com>
2024-01-28 14:53:19 -0700

    Added custom user + drf spectacular

--- Dockerfile
+++ Dockerfile
@@ -29,3 +29,5 @@ RUN python -m venv /py && \
 ENV PATH="/py/bin:$PATH"
 
 USER django-user
+
+USER daemon
--- app/app/calc.py
+++ app/app/calc.py
@@ -6,3 +6,7 @@ Calculator functions
 def add(x, y):
     """bla bla bla"""
     return x + y
+
+def subtract(x,y):
+    """bla bla bla"""
+    return x - y
